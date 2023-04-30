7d8c5290792bc0a1d011c6e22ca63b6c4398c90e
redtbs <132199824+redtbs@users.noreply.github.com>
2023-04-30 17:00:36 +0100

    2 files modified

--- src/app/project-list/project-list.component.ts
+++ src/app/project-list/project-list.component.ts
@@ -1,16 +1,4 @@
-/*import { Component } from '@angular/core';
 
-import { projects } from '../projects';
-
-
-export class ProjectListComponent {
-  projects = projects;
-
-  share() {
-    window.alert('The project has been shared!');
-  }
-}
-*/
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
@@ -39,21 +27,7 @@ constructor(private http: HttpClient) {
 }
 
 loadProjects() {
- /* const url = 'https://dev.azure.com/{organization}/{project}/_apis/projects?api-version=6.0';
-  const headers = { 'Authorization': 'Basic ' + btoa(':' + '{personal access token}') };*/
- /* this.http.get(url, { headers }).subscribe(
-    (data: any) => {
-      this.projects = data.value.map((project: any) => ({
-        name: project.name,
-        id: project.id,
-        url: project.url,
-        status: project.status,
-        revision: project.revision
-      }));
-    },
-
-    */
-
+ 
     this.http.get<Project[]>('assets/projects.json').subscribe(
       data => {
         this.projects = data;
